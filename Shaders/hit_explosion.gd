fd96d412bc8db0976a6cf53fcbfdb3782b8f91f2
brodie-sm1th <0363029@schoolsnet.act.edu.au>
2026-03-27 11:12:47 +1100

    Bullet Hole Update and Test Shader

--- Shaders/hit_explosion.gd
+++ Shaders/hit_explosion.gd
@@ -20,6 +20,9 @@ func _ready() -> void:
 	explosion.emitting = true
 	sphere.emitting = true
 	smoke.emitting = true
+	hole.rotate_y(randf() * 2 * PI)
+	hole.rotate_x(randf_range(-1, 1) * PI / 4)
+	hole.rotate_z(randf_range(-1, 1) * PI / 4)
 
 
 
@@ -29,6 +32,7 @@ func _process(delta: float) -> void:
 		if smoke:
 			smoke.amount_ratio = (lifetime_rat ** 4) * 0.9 + lifetime_rat ** 0.2 * 0.1
 		if hole:
-			hole.albedo_mix = lifetime_rat
+			hole.albedo_mix = lifetime_rat ** 0.75
 			var lifetime_ratmod = (lifetime_rat ** 3) * 0.9 + lifetime_rat ** 0.2 * 0.1
 			hole.modulate = Color(lifetime_ratmod, lifetime_ratmod, lifetime_ratmod, 1)
+			hole.scale = lifetime_rat ** 0.5 * Vector3.ONE
